15c103596951f5cbbca365e2d94e2abf528728a6
Adrian T <adrian.thorburn@gmail.com>
2024-02-15 17:12:25 +1100

    move swiper to component, simplify homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -22,33 +22,7 @@ export default function Home() {
      
      <div>
       <div className="relative z-[3]">
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-        allowTouchMove={false}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
-        loop={true}
-        effect={'fade'}
-        modules={[EffectFade]}
-
-      >
-        {desktopImages.map((image, index) => (
-          <div key={index} className="m-2">
-            <SwiperSlide key={index}> 
-              <Image
-                src={image}
-                width={1920}  // Set the actual width of the image
-                height={1080} // Set the actual height of the image
-              /> 
-            </SwiperSlide>
-           
-          </div>
-        ))}
-       
-       <SwiperNavButton />
-        {/* <button class="absolute top-[0px] left-[0px] z-[33] h-[100vh] w-[100vw]"></button> */}
-      </Swiper>
+        
       </div>
 
      </div>
